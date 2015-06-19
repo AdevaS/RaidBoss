478771c9ef2d4dc9fbfcffb57f1ab6662aeb2798
Adevanir Junior <adevanir@gmail.com>
2015-06-19 17:38:08 -0300

    I18n configuration complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
+  get '/:locale' => 'home#index'
 
   # You can have the root of your site routed with "root"
   root to: 'home#index'
